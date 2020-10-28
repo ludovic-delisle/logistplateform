5dd79a43fef5147e1673513e8f7b80ab71bf074b
ljerome <ljerome@student.ethz.ch>
2020-10-28 16:02:24 +0100

    mieux avec HasMap<vehicle, LinkedList<Task>>

--- centralized/src/template/NextTasks.java
+++ centralized/src/template/NextTasks.java
@@ -1,7 +1,9 @@
 package template;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -13,74 +15,94 @@ import logist.task.Task;
 import logist.task.TaskSet;
 
 public class NextTasks {
-	private HashMap<Task,Task> nextTask;
-	private HashMap<Task, Vehicle> nextTaskVehicle;
-	private HashMap<Task,Integer> timeMap;
+	private HashMap<Vehicle, LinkedList<Task>> nextTask;
 	
 	public NextTasks() {
-		this.nextTask = new HashMap<Task,Task>();
-		this.nextTaskVehicle = new HashMap<Task, Vehicle>();
-		this.timeMap = new HashMap<Task, Integer>();
+		this.nextTask = new HashMap<Vehicle,LinkedList<Task>>();
 	}
 	
 	public NextTasks(NextTasks n) {
-		this.nextTask = new HashMap<Task,Task>(n.nextTask);
-		this.nextTaskVehicle = new HashMap<Task, Vehicle>(n.nextTaskVehicle);
-		this.timeMap = new HashMap<Task, Integer>(n.timeMap);
+		this.nextTask = new HashMap<Vehicle,LinkedList<Task>>(n.nextTask);
 	}
 	
 	public NextTasks(List<Vehicle> vehicles, TaskSet tasks) {
-		this.nextTask = new HashMap<Task,Task>();
-		this.nextTaskVehicle = new HashMap<Task, Vehicle>();
-		this.timeMap = new HashMap<Task, Integer>();
+		LinkedList<Task> ll_t = new LinkedList<Task>();
+		HashMap<Vehicle, LinkedList<Task>> hm = new HashMap<Vehicle,LinkedList<Task>>();
 		
 		Iterator<Vehicle> it = vehicles.iterator();
 		Vehicle v = it.next();
 		for(Task t: tasks) {
-			if(v.getCurrentTasks().weightSum() + t.weight > v.capacity())	v = it.next();
-			nextTaskVehicle.put(t, v);
+			if(v.getCurrentTasks().weightSum() + t.weight > v.capacity()) ll_t.add(t);
+			else {
+				hm.put(v, new LinkedList<Task>(ll_t));
+				v = it.next();
+				ll_t.clear();
+			}
 		}
+		this.nextTask = hm;
 	}
 	
 	public Task get(Task t) {
-		return nextTask.get(t);
+		for(Vehicle v: nextTask.keySet()) {
+			LinkedList<Task> ll_t = nextTask.get(v);
+			int idx = ll_t.indexOf(t);
+			if(idx > 0) return ll_t.get(idx + 1);
+		}
+		return null; 
 	}
 	
-	public Set<Task> get(Vehicle v) {
-		return nextTaskVehicle.entrySet()
-	              .stream()
-	              .filter(entry -> Objects.equals(entry.getValue(), v))
-	              .map(Map.Entry::getKey)
-	              .collect(Collectors.toSet());
+	public List<Task> get(Vehicle v) {
+		return nextTask.get(v);
 	}
 	
 	public Integer getTime(Task t) {
-		return timeMap.get(t);
+		for(Vehicle v: nextTask.keySet()) {
+			LinkedList<Task> ll_t = nextTask.get(v);
+			int idx = ll_t.indexOf(t);
+			if(idx > 0) return idx+1;
+		}
+		return 0;
 	}
 	
 	public Integer size() {
-		return nextTask.size() + nextTaskVehicle.size();
-	}
-	
-	public void remove(Task t) {
-		nextTask.remove(t);
+		int nb_tasks=0;
+		for(Vehicle v: nextTask.keySet()) {
+			nb_tasks += nextTask.get(v).size();
+		}
+		return nextTask.size() + nb_tasks;
 	}
 	
 	public void put(Task t1, Task t2) {
-		nextTask.put(t1, t2);
+		for(Vehicle v: nextTask.keySet()) {
+			LinkedList<Task> ll_t = nextTask.get(v);
+			int idx = ll_t.indexOf(t1);
+			if(idx > 0) {
+				ll_t.add(idx, t2);
+			}
+		}
 	}
 	
 	public void swap_task_order(Task t) {
-		Task old_next_task = nextTask.remove(t);
-		nextTask.put(old_next_task, t);
+		for(Vehicle v: nextTask.keySet()) {
+			LinkedList<Task> ll_t = nextTask.get(v);
+			int idx = ll_t.indexOf(t);
+			if(idx > 0) {
+				Collections.swap(ll_t, idx, idx+1);
+			}
+		}
 	}
 	
 	public Vehicle getVehicle(Task t) {
-		return nextTaskVehicle.get(t);
+		for(Vehicle v: nextTask.keySet()) {
+			LinkedList<Task> ll_t = nextTask.get(v);
+			int idx = ll_t.indexOf(t);
+			if(idx > 0) return v;
+		}
+		return null; 
 	}
 	
 	public void swap_task_vehicle(Task t, Vehicle v1, Vehicle v2) {
-		if(nextTaskVehicle.remove(t, v1)) nextTaskVehicle.put(t, v2);
+		if(nextTask.get(v1).remove(t)) nextTask.get(v2).addFirst(t);
 	}
 	
 }
